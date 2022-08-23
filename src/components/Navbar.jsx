ce5da5d9f973154ba3ab478a9feb3df6f503b016
Ayushsingh07 <nqu7069@gmail.com>
2022-08-23 23:30:56 +0530

    firstday progres

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar =() => {
+  return (
+    <div className='text-white'>
+        <h1 className='w-full text-3xl font-bold text-[#009f03]'>hello world</h1>
+        <ul>
+          <li> Home</li>
+          <li> company</li>
+          <li> resources </li>
+          <li> contact</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
